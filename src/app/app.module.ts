10abb20c1dbb461102cd34fbe18d160cc0380e8f
MGonzalezx <martingonzalezt97@gmail.com>
2023-09-11 17:15:18 -0300

    TP-primerCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { InfoAlumnoComponent } from './components/info-alumno/info-alumno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    InfoAlumnoComponent
   ],
   imports: [
     BrowserModule,
